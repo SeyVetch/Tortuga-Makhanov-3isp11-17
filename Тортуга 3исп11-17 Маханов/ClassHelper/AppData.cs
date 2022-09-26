ba6d9bcc1af64a770c7f722871b79701ea428b0b
Саша Маханов <dendiherobrine@gmail.com>
2022-09-26 17:00:06 +0300

    Added Database and images

---
+++ Тортуга 3исп11-17 Маханов/ClassHelper/AppData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Тортуга_3исп11_17_Маханов.ClassHelper
+{
+    class AppData
+    {
+        public static EF.Entities Context { get; } = new EF.Entities();
+    }
+}
